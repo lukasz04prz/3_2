0cffa381eec74666ced5556505b16c4985696b91
lukasz04prz <lukasz04prz@gmail.com>
2025-11-12 08:53:52 +0000

    Com3_2.3

--- script.js
+++ script.js
@@ -39,4 +39,20 @@ var api_key = "WyVCmQTTsZvazOCsrmExBNaNOvTQeJzI";
         l.appendChild(row);
       })
     });
+
+  fetch(`https://corsproxy.io/?${encodeURIComponent("https://www.ncei.noaa.gov/cdo-web/api/v2/datacategories?enddate=2023-01-01")}`, {
+    headers: {
+      "token": api_key,
+    },
+  }).then(response => response.json())
+    .then(result => {
+      result.results.forEach(station => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+             <td>${station.id}</td>
+             <td>${station.name}</td>
+         `;
+        c3.appendChild(row);
+      })
+    });
 })();
